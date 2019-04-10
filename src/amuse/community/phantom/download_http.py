1d6eb01b83ab46aafe428c5ae1306f571cd69f70
Steven Rieder <steven@rieder.nl>
2019-04-10 13:10:21 +0100

    Pull most recent phantom amuse build

--- src/amuse/community/phantom/download_http.py
+++ src/amuse/community/phantom/download_http.py
@@ -78,7 +78,7 @@ class MyFancyUrlopener(urllib.FancyURLopener):
 class GetCodeFromHttp(object):
     url_template = "https://bitbucket.org/danielprice/phantom/get/{version}.zip"
     filename_template = "{version}.zip"
-    version = "3923bbb681f1"
+    version = "cb3d690e1a47"
 
     def directory(self):
         return os.path.abspath(os.path.dirname(__file__))
@@ -140,7 +140,7 @@ def new_option_parser():
 
     result.add_option(
         "--version",
-        default='3923bbb681f1',
+        default='cb3d690e1a47',
         dest="version",
         help="git revision to download",
         type="string"
