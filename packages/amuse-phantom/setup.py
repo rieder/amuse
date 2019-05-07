9ff010b7f877cb25e684f6c9674d698847e3bb13
Steven Rieder <steven@rieder.nl>
2019-05-07 13:53:35 +0100

    Updates to setup

--- packages/amuse-phantom/setup.py
+++ packages/amuse-phantom/setup.py
@@ -2,6 +2,9 @@ import sys
 import os
 
 from setuptools import setup
+
+import support
+support.use("system")
 from support.setup_codes import setup_commands
 
 name = 'amuse-phantom'
@@ -17,9 +20,9 @@ install_requires = [
     'nose>=0.11.1',
     'mpi4py>=1.1.0',
     'h5py>=1.1.0',
-    'amuse-framework>=12.0.0pre7',
+    'amuse-framework>=12.0.0rc3',
 ]
-description = 'The Astrophysical Multipurpose Software Environment'
+description = 'The Astrophysical Multipurpose Software Environment - Phantom'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 long_description_content_type = "text/markdown"
@@ -32,6 +35,7 @@ classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: POSIX',
+    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: C',
@@ -58,7 +62,7 @@ setup(
     url=url,
     author_email=author_email,
     author=author,
-    license=license_,    
+    license=license_,
     description=description,
     long_description=long_description,
     long_description_content_type=long_description_content_type,
@@ -69,5 +73,4 @@ setup(
     packages=packages,
     package_data=package_data,
     data_files=all_data_files,
-    scripts=["amusifier"],
 )
