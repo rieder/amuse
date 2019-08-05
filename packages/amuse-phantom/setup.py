00b96c1477384d6a4659faae6ca75e326c2e253e
Steven Rieder <steven@rieder.nl>
2019-08-05 10:54:33 +0100

    add patch version

--- packages/amuse-phantom/setup.py
+++ packages/amuse-phantom/setup.py
@@ -1,4 +1,8 @@
 import os
+from support.version import (
+    major_version, minor_version, main_version
+)
+from support.classifiers import classifiers
 
 from setuptools import setup
 
@@ -6,10 +10,11 @@ import support
 support.use("system")
 from support.setup_codes import setup_commands
 
-os.environ["DOWNLOAD_ONLY"] = "1"
-
 name = 'amuse-phantom'
-version = "12.0.0b13"
+micro_version = 0
+patch_version = "b13"
+version = "%i.%i.%i.%s" % (major_version, minor_version, micro_version,
+                           patch_version)
 author = 'The AMUSE team'
 author_email = 'info@amusecode.org'
 license_ = "Apache License 2.0"
@@ -21,31 +26,13 @@ install_requires = [
     'nose>=0.11.1',
     'mpi4py>=1.1.0',
     'h5py>=1.1.0',
-    'amuse-framework>=12.0.0',
+    'amuse-framework>=%s' % (main_version),
 ]
 description = 'The Astrophysical Multipurpose Software Environment - Phantom'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 long_description_content_type = "text/markdown"
-classifiers = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Intended Audience :: End Users/Desktop',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: POSIX',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: C',
-    'Programming Language :: C++',
-    'Programming Language :: Fortran',
-    'Topic :: Scientific/Engineering :: Astronomy',
-]
+os.environ["DOWNLOAD_ONLY"] = "1"
 
 extensions = []
 
