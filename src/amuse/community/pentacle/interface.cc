f85ad55df3960ea667c8da72f1256702b9b9b110
Steven Rieder <steven@rieder.nl>
2019-12-02 10:23:21 +0000

    interface changes + tests

--- src/amuse/community/pentacle/interface.cc
+++ src/amuse/community/pentacle/interface.cc
@@ -84,8 +84,8 @@ int sync_particles_to_master(){
     // There is no real danger of running out of memory (except in very special
     // cases) because AMUSE requires the particles to be on the master process
     // initially anyway...
-    //const auto n_loc = system_soft.getNumberOfParticleLocal();
-    //const auto n_glb = system_soft.getNumberOfParticleGlobal();
+    const auto n_loc = system_soft.getNumberOfParticleLocal();
+    const auto n_glb = system_soft.getNumberOfParticleGlobal();
     FpSoft * fp_gather = new FpSoft[n_glb];
     PS::S32 n_recv_tot = 0;
     PS::Comm::gatherVAll(&system_soft[0], n_loc, fp_gather, n_recv_tot);
@@ -102,7 +102,7 @@ int initialize_code(){
     //PARAM_SET.time_end_ = 10.0;
     PARAM_SET.time_end_ = 0.125;
     PARAM_SET.eps_    = 0.1;
-    PARAM_SET.eps_sq_ = PARAM_SET.eps_ * PARAM_SET.eps_;
+    PARAM_SET.eps_sq_ = 0.01; //PARAM_SET.eps_ * PARAM_SET.eps_;
 
     PARAM_SET.theta_    = theta;
     PARAM_SET.dt_soft_  = 1.0 / 256.0;
@@ -166,7 +166,7 @@ int get_radius(int id, double* radius){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     *radius = system_soft[i].r_out;
     return 0;
 }
@@ -176,12 +176,14 @@ int set_radius(int id, double radius){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
-    system_soft[i].r_out = radius;
-    system_soft[i].r_in  = system_soft[i].r_out * 0.1; // inner cutoff radius for ptree-pp
-    const PS::F64 r_buf = 1.5 * system_soft[i].r_out;
-    system_soft[i].r_search  = system_soft[i].r_out + r_buf;
-    return 0;
+    if (i == -1) return -3;
+    if(PS::Comm::getRank()==0) {
+        system_soft[i].r_out = radius;
+        system_soft[i].r_in  = system_soft[i].r_out * 0.1; // inner cutoff radius for ptree-pp
+        const PS::F64 r_buf = 1.5 * system_soft[i].r_out;
+        system_soft[i].r_search  = system_soft[i].r_out + r_buf;
+        return 0;
+    }
 }
 
 int cleanup_code(){
@@ -189,6 +191,22 @@ int cleanup_code(){
 }
 
 int evolve_model(double time){
+    if (started_running != 1) {
+        // Only once
+        //KickOmp(system_soft, PARAM_SET.dt_soft_*0.5);
+
+        PS::Comm::barrier();
+        if(PS::Comm::getRank()==0)
+            std::cerr<<"start connect cluster and integrate"<<std::endl;
+        
+        // Only once
+        //system_cluster.connectClusterAndIntegrate(system_soft, tree_soft, DriveOneCluster<FpSoft>, DriveMulCluster<FpSoft>, false);
+
+        PS::Comm::barrier();
+        if(PS::Comm::getRank()==0)
+            std::cerr<<"finish connect cluster and integrate"<<std::endl;
+    }
+    
     started_running = 1;
     Energy eng_init(system_soft);
     std::cerr<<"eng_init:"<<std::endl;
@@ -198,24 +216,26 @@ int evolve_model(double time){
     PARAM_SET.time_end_ = time;
     PS::S64 n_loop = 0;
 
-    std::cerr<<"GLOBALS:"<<std::endl;
-    std::cerr<<"n_glb = "<<n_glb<<std::endl;
-    std::cerr<<"n_loc = "<<n_loc<<std::endl;
-    std::cerr<<"time_sys = "<<time_sys<<std::endl;
-    std::cerr<<"n_grp_limit = "<<n_grp_limit<<std::endl;
-    std::cerr<<"n_leaf_limit = "<<n_leaf_limit<<std::endl;
-    std::cerr<<"n_smp_ave = "<<n_smp_ave<<std::endl;
-    std::cerr<<"theta = "<<theta<<std::endl;
-    std::cerr<<std::endl;
-    std::cerr<<"PARAMETERS:"<<std::endl;
-    std::cerr<<"time_end = "<<PARAM_SET.time_end_<<std::endl;
-    std::cerr<<"eps = "<<PARAM_SET.eps_<<std::endl;
-    std::cerr<<"eps_sq = "<<PARAM_SET.eps_sq_<<std::endl;
-    std::cerr<<"theta = "<<PARAM_SET.theta_<<std::endl;
-    std::cerr<<"dt_soft = "<<PARAM_SET.dt_soft_<<std::endl;
-    std::cerr<<"eta = "<<PARAM_SET.eta_<<std::endl;
-    std::cerr<<"eta_s = "<<PARAM_SET.eta_s_<<std::endl;
-    std::cerr<<"dt_hard_limit = "<<PARAM_SET.dt_hard_limit_<<std::endl;
+    /*
+     * std::cerr<<"GLOBALS:"<<std::endl;
+     * std::cerr<<"n_glb = "<<n_glb<<std::endl;
+     * std::cerr<<"n_loc = "<<n_loc<<std::endl;
+     * std::cerr<<"time_sys = "<<time_sys<<std::endl;
+     * std::cerr<<"n_grp_limit = "<<n_grp_limit<<std::endl;
+     * std::cerr<<"n_leaf_limit = "<<n_leaf_limit<<std::endl;
+     * std::cerr<<"n_smp_ave = "<<n_smp_ave<<std::endl;
+     * std::cerr<<"theta = "<<theta<<std::endl;
+     * std::cerr<<std::endl;
+     * std::cerr<<"PARAMETERS:"<<std::endl;
+     * std::cerr<<"time_end = "<<PARAM_SET.time_end_<<std::endl;
+     * std::cerr<<"eps = "<<PARAM_SET.eps_<<std::endl;
+     * std::cerr<<"eps_sq = "<<PARAM_SET.eps_sq_<<std::endl;
+     * std::cerr<<"theta = "<<PARAM_SET.theta_<<std::endl;
+     * std::cerr<<"dt_soft = "<<PARAM_SET.dt_soft_<<std::endl;
+     * std::cerr<<"eta = "<<PARAM_SET.eta_<<std::endl;
+     * std::cerr<<"eta_s = "<<PARAM_SET.eta_s_<<std::endl;
+     * std::cerr<<"dt_hard_limit = "<<PARAM_SET.dt_hard_limit_<<std::endl;
+     */
         
     // Should fix this, but the 0.0001 here is to prevent precision errors from
     // letting the code run for a full timestep longer than intended.
@@ -303,18 +323,19 @@ int evolve_model(double time){
         n_loop++;
         //if(n_loop == 50) break;
     }
-
+    if (particles_on_master == false) {
+        sync_particles_to_master();
+    }
     return 0;
 }
 
 int get_position(int id, double* x, double* y, double* z){
-    std::cerr<<"get_position"<<std::endl;
     if (particles_on_master == false) {
         std::cerr<<"sync_particles_to_master"<<std::endl;
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     //PS::S32 i = id;
     if(PS::Comm::getRank()==0) {
         *x = system_soft[i].pos.x;
@@ -329,12 +350,14 @@ int get_velocity(int id, double* vx, double* vy, double* vz){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     //PS::S32 i = id;
-    *vx = system_soft[i].vel.x;
-    *vy = system_soft[i].vel.y;
-    *vz = system_soft[i].vel.z;
-    return 0;
+    if(PS::Comm::getRank()==0) {
+        *vx = system_soft[i].vel.x;
+        *vy = system_soft[i].vel.y;
+        *vz = system_soft[i].vel.z;
+        return 0;
+    }
 }
 
 int new_particle(int* id, double mass, double x, double y, double z, double vx, double vy, double vz, double radius){
@@ -342,44 +365,53 @@ int new_particle(int* id, double mass, double x, double y, double z, double vx, 
     // Solved by other community codes in these ways:
     // Hermite:  if(mpi_rank) {return 0;}
     // Bonsai2:  calls getCurrentStateToHost
-    if(PS::Comm::getRank()==0){
-        //std::cerr<<"Adding particle"<<std::endl;
-        FpSoft particle;
-        PS::S64 pid = unique_particles;
-        unique_particles++;
-        n_glb++;
-        n_loc++;
-        
-        //system_soft.setNumberOfParticleLocal(n_loc);
-        //PS::S32 i = unique_particles;
-
-        particle.id = pid;
-        particle.mass = mass;
-        particle.pos.x = x;
-        particle.pos.y = y;
-        particle.pos.z = z;
-        particle.vel.x = vx;
-        particle.vel.y = vy;
-        particle.vel.z = vz;
-        //particle.r_out = PARAM_SET.dt_soft_ * 2.0;  // cutoff radius for ptree-pp
-        particle.r_out = radius;  // cutoff radius for ptree-pp
-        particle.r_in  = particle.r_out * 0.1; // inner cutoff radius for ptree-pp
-        const PS::F64 r_buf = 1.5 * particle.r_out;
-        particle.r_search  = particle.r_out + r_buf;
-
-        particle_id[pid] = system_soft.getNumberOfParticleGlobal();
-
-        system_soft.addOneParticle(particle);
-        *id = pid;
-        //std::cerr<<"n_glb="<<n_glb<<std::endl;
-        //std::cerr<<"n_loc="<<n_glb<<std::endl;
+    if(PS::Comm::getRank()){
         return 0;
     }
-    return -1;
+    //if (particles_on_master == false) {
+    //    sync_particles_to_master();
+    //}
+    std::cerr<<"new particle on rank "<<PS::Comm::getRank()<<std::endl<<std::flush;
+    //std::cerr<<"Adding particle"<<std::endl;
+    FpSoft particle;
+    unique_particles++;
+    PS::S64 pid = unique_particles;
+    std::cerr<<"check 0"<<std::endl<<std::flush;
+    //system_soft.setNumberOfParticleLocal(n_loc);
+    //PS::S32 i = unique_particles;
+
+    particle.id = pid;
+    particle.mass = mass;
+    particle.pos.x = x;
+    particle.pos.y = y;
+    particle.pos.z = z;
+    particle.vel.x = vx;
+    particle.vel.y = vy;
+    particle.vel.z = vz;
+    //particle.r_out = PARAM_SET.dt_soft_ * 2.0;  // cutoff radius for ptree-pp
+    particle.r_out = radius;  // cutoff radius for ptree-pp
+    particle.r_in  = particle.r_out * 0.1; // inner cutoff radius for ptree-pp
+    const PS::F64 r_buf = 1.5 * particle.r_out;
+    particle.r_search  = particle.r_out + r_buf;
+
+    std::cerr<<"check 0.5"<<std::endl<<std::flush;
+    particle_id[pid] = n_glb; //system_soft.getNumberOfParticleGlobal();
+    std::cerr<<"check 1"<<std::endl<<std::flush;
+
+    system_soft.addOneParticle(particle);
+    n_glb++;
+    n_loc++;
+    std::cerr<<"check 2"<<std::endl<<std::flush;
+    *id = pid;
+    //std::cerr<<"n_glb="<<n_glb<<std::endl;
+    //std::cerr<<"n_loc="<<n_glb<<std::endl;
+    //return -1;
     //else{
         //std::cerr<<"Not adding particle"<<std::endl;
         //return 0;
     //}
+    std::cerr<<"check 3"<<std::endl<<std::flush;
+    return 0;
 }
     
 int set_position(int id, double x, double y, double z){
@@ -387,7 +419,7 @@ int set_position(int id, double x, double y, double z){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     //PS::S32 i = id;
     system_soft[i].pos.x = x;
     system_soft[i].pos.y = y;
@@ -400,7 +432,7 @@ int set_velocity(int id, double vx, double vy, double vz){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     //PS::S32 i = id;
     system_soft[i].vel.x = vx;
     system_soft[i].vel.y = vy;
@@ -455,7 +487,7 @@ int delete_particle(int id){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     FpSoft p = system_soft[i];
     PS::S32 n_remove = 1;
     PS::S32 *idx[n_remove] = { new PS::S32[n_remove] };
@@ -466,6 +498,10 @@ int delete_particle(int id){
 }
 
 int commit_particles(){
+    //if (particles_on_master == false) {
+    //    sync_particles_to_master();
+    //}
+
     dinfo.decomposeDomainAll(system_soft);
     system_soft.exchangeParticle(dinfo);
     particles_on_master = false;
@@ -495,20 +531,22 @@ int commit_particles(){
     }
     
     // Only once
-    KickOmp(system_soft, PARAM_SET.dt_soft_*0.5);
+    //KickOmp(system_soft, PARAM_SET.dt_soft_*0.5);
 
-    PS::Comm::barrier();
-    if(PS::Comm::getRank()==0)
-        std::cerr<<"start connect cluster and integrate"<<std::endl;
+    //PS::Comm::barrier();
+    //if(PS::Comm::getRank()==0)
+    //    std::cerr<<"start connect cluster and integrate"<<std::endl;
     
     // Only once
-    system_cluster.connectClusterAndIntegrate(system_soft, tree_soft, DriveOneCluster<FpSoft>, DriveMulCluster<FpSoft>, false);
+    //system_cluster.connectClusterAndIntegrate(system_soft, tree_soft, DriveOneCluster<FpSoft>, DriveMulCluster<FpSoft>, false);
 
-    PS::Comm::barrier();
-    if(PS::Comm::getRank()==0)
-        std::cerr<<"finish connect cluster and integrate"<<std::endl;
-
-    return 0;
+    //PS::Comm::barrier();
+    //if(PS::Comm::getRank()==0)
+    //    std::cerr<<"finish connect cluster and integrate"<<std::endl;
+    
+    //if(PS::Comm::getRank()==0) {
+        return 0;
+    //}
 }
 
 int get_acceleration(int id, double* ax, double* ay, double* az){
@@ -581,7 +619,7 @@ int get_mass(int id, double* mass){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     *mass = system_soft[i].mass;
     return 0;
 }
@@ -591,7 +629,7 @@ int set_mass(int id, double mass){
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     system_soft[i].mass = mass;    
     return 0;
 }
@@ -601,7 +639,7 @@ int get_state(int id, double* mass, double* x, double* y, double* z, double* vx,
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id); // This must be a local map
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     *mass = system_soft[i].mass;
     *x = system_soft[i].pos.x;
     *y = system_soft[i].pos.y;
@@ -618,7 +656,7 @@ int set_state(int id, double mass, double x, double y, double z, double vx, doub
         sync_particles_to_master();
     }
     PS::S32 i = get_particle_id(id);
-    if (i == -1) return -1;
+    if (i == -1) return -3;
     
     system_soft[i].mass = mass;
     system_soft[i].pos.x = x;
--- src/amuse/community/pentacle/test_pentacle.py
+++ src/amuse/community/pentacle/test_pentacle.py
@@ -1,21 +1,307 @@
+import math
+
 from amuse.test.amusetest import TestWithMPI
+
+from amuse.datamodel import Particles
+from amuse.units import units, constants, nbody_system
+from amuse.ic.plummer import new_plummer_model
 
 from .interface import PentacleInterface
 from .interface import Pentacle
 
+
+default_options = dict() # redirection="none")
+
 class PentacleInterfaceTests(TestWithMPI):
     
-    def xtest1(self):
-        instance = PentacleInterface()
-        result,error = instance.echo_int(12)
-        self.assertEquals(error, 0)
-        self.assertEquals(result, 12)
+    def test_new_instance(self):
+        instance = self.new_instance_of_an_optional_code(
+            PentacleInterface,
+            **default_options
+        )
+        self.assertEqual(0, instance.initialize_code())
+        instance.stop()
+
+    def test_new_particles(self):
+        instance = self.new_instance_of_an_optional_code(
+            PentacleInterface,
+            **default_options
+        )
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+
+        index, error = instance.new_particle(
+            mass=11.0, x=0.0, y=0.0, z=0.0, vx=0.0, vy=0.0, vz=0.0, radius=0.1
+        )
+        self.assertEqual(0, error)
+        self.assertEqual(1, index)
+        index, error = instance.new_particle(
+            mass=21.0, x=10.0, y=0.0, z=0.0, vx=10.0, vy=0.0, vz=0.0, radius=0.2
+        )
+        self.assertEqual(0, error)
+        self.assertEqual(2, index)
+        self.assertEqual(0, instance.commit_particles())
+        retrieved_state1 = instance.get_state(1)
+        retrieved_state2 = instance.get_state(2)
+        self.assertEqual(0, retrieved_state1['__result'])
+        self.assertEqual(0, retrieved_state2['__result'])
+        self.assertEqual(11.0, retrieved_state1['mass'])
+        self.assertEqual(21.0, retrieved_state2['mass'])
+        self.assertEqual(0.0, retrieved_state1['x'])
+        self.assertEqual(10.0, retrieved_state2['x'])
+
+        self.assertEqual(0, instance.cleanup_code())
+        instance.stop()
+
+    def test_particle_property_setters_getters(self):
+        instance = self.new_instance_of_an_optional_code(
+            PentacleInterface,
+            **default_options
+        )
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+        self.assertEqual([1, 0], list(instance.new_particle(0.01,  1, 0, 0,  0, 1, 0, 0.1).values()))
+        self.assertEqual([2, 0], list(instance.new_particle(0.02, -1, 0, 0,  0,-1, 0, 0.1).values()))
+        self.assertEqual(0, instance.commit_particles())
+        
+        # getters
+        mass, result = instance.get_mass(1)
+        self.assertAlmostEqual(0.01, mass)
+        self.assertEqual(0,result)
+        radius, result = instance.get_radius(2)
+        self.assertAlmostEqual(0.1, radius)
+        self.assertEqual(0,result)
+        self.assertEqual(-3, instance.get_mass(3)['__result']) # Particle not found
+        self.assertEqual([ 1, 0, 0,  0], list(instance.get_position(1).values()))
+        self.assertEqual([-1, 0, 0,  0], list(instance.get_position(2).values()))
+        self.assertEqual([ 0, 1, 0,  0], list(instance.get_velocity(1).values()))
+        self.assertEqual([ 0,-1, 0,  0], list(instance.get_velocity(2).values()))
+        
+        # setters
+        self.assertEqual(0, instance.set_state(1, 0.01, 1,2,3, 4,5,6, 0.1))
+        self.assertEqual([0.01, 1.0,2.0,3.0, 4.0,5.0,6.0, 0.1, 0], list(instance.get_state(1).values()))
+        self.assertEqual(0, instance.set_mass(1, 0.02))
+        self.assertEqual([0.02, 1.0,2.0,3.0, 4.0,5.0,6.0, 0.1, 0], list(instance.get_state(1).values()))
+        self.assertEqual(0, instance.set_radius(1, 0.2))
+        self.assertEqual([0.02, 1.0,2.0,3.0, 4.0,5.0,6.0, 0.2, 0], list(instance.get_state(1).values()))
+        self.assertEqual(0, instance.set_position(1, 10,20,30))
+        self.assertEqual([0.02, 10.0,20.0,30.0, 4.0,5.0,6.0, 0.2, 0], list(instance.get_state(1).values()))
+        self.assertEqual(0, instance.set_velocity(1, 40,50,60))
+        self.assertEqual([0.02, 10.0,20.0,30.0, 40.0,50.0,60.0, 0.2, 0], list(instance.get_state(1).values()))
+
+        self.assertEqual(0, instance.cleanup_code())
+        instance.stop()
+
+    def test_parameters(self):
+        instance = self.new_instance_of_an_optional_code(PentacleInterface, **default_options)
+        self.assertEqual(0, instance.initialize_code())
+        
+        self.assertEqual([1.0e-2, 0], list(instance.get_eps2().values()))
+        
+        self.assertEqual(0, instance.set_eps2(0.2))
+        self.assertEqual([0.2, 0], list(instance.get_eps2().values()))
+        
+        self.assertEqual([0.1, 0], list(instance.get_eta().values()))
+        
+        self.assertEqual(0, instance.set_eta(0.01))
+        self.assertEqual([0.01, 0], list(instance.get_eta().values()))
+        
+        self.assertEqual(0, instance.commit_parameters())
+        self.assertEqual(0, instance.cleanup_code())
         instance.stop()
 
 class PentacleTests(TestWithMPI):
 
-    def test_startstop(self):
-        instance = Pentacle()
-        parameters = instance.parameters
-        print(parameters)
+    default_converter = nbody_system.nbody_to_si(1.0e4 | units.MSun, 1.0 | units.AU)
+
+    def new_sun_earth_system(self):
+        particles = Particles(2)
+        particles.mass = [1.0, 3.0037e-6] | units.MSun
+        particles.position = [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]] | units.AU
+        particles.velocity = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]] | units.km / units.s
+        particles[1].vy = (constants.G * particles.total_mass() / (1.0 | units.AU)).sqrt()
+        return particles
+
+    def test_initialization(self):
+        instance = self.new_instance_of_an_optional_code(Pentacle, self.default_converter, **default_options)
+        instance.initialize_code()
+        instance.commit_parameters()
+        instance.cleanup_code()
         instance.stop()
+
+    def test_parameters(self):
+        instance = self.new_instance_of_an_optional_code(Pentacle, self.default_converter, **default_options)
+        instance.initialize_code()
+        
+        self.assertEqual(
+            instance.parameters.epsilon_squared, 
+            instance.unit_converter.to_si(1.0e-2 | nbody_system.length**2)
+        )
+        self.assertEqual(instance.parameters.time_step_parameter, 0.1)
+        self.assertEqual(
+            instance.parameters.time_step,
+            instance.unit_converter.to_si(1/256 | nbody_system.time)
+        )
+        
+        # for par, value in [
+        #         # ('opening_angle', 0.4),
+        # ]:
+        #     self.assertEqual(instance.unit_converter.to_si(value), 
+        #         getattr(instance.parameters, par))
+        #         
+        #     if hasattr(value, 'unit'):
+        #         new_value = 3.0 | value.unit
+        #     else:
+        #         new_value = 3.0
+        #         
+        #     setattr(instance.parameters, par, new_value)
+        #     self.assertEqual(instance.unit_converter.to_si(new_value),
+        #         getattr(instance.parameters, par))
+        
+        instance.commit_parameters()
+        p = instance.parameters
+        
+        instance.stop()
+
+    def test_instance(self):
+        instance = self.new_instance_of_an_optional_code(Pentacle, self.default_converter, **default_options)
+        instance.initialize_code()
+        instance.commit_parameters()
+        instance.particles.add_particles(self.new_sun_earth_system())
+        instance.commit_particles()
+        
+        self.assertAlmostEqual(instance.particles.mass, [1.0, 3.0037e-6] | units.MSun)
+        self.assertAlmostEqual(instance.particles.position, 
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]] | units.AU)
+        self.assertAlmostEqual(instance.particles.velocity, 
+            [[0.0, 0.0, 0.0], [0.0, 29.7885, 0.0]] | units.km / units.s, 3)
+        
+        instance.cleanup_code()
+        instance.stop()
+
+    def test_evolve_oneparticle(self):
+        particles = Particles(1)
+        particles.mass = 1.0 | units.MSun
+        # instance.parameters.rcut_out_star_star = 10.0 | units.AU
+        # particles.radius = 10.0 | units.AU
+        particles.position = [[0.0, 0.0, 0.0]] | units.AU
+        particles.velocity = [[1.0, 0.0, 0.0]] | units.AU / units.yr
+        
+        converter = nbody_system.nbody_to_si(1.0 | units.MSun, 1.0 | units.AU)
+        instance = Pentacle(converter) #self.new_instance_of_an_optional_code(Pentacle, converter, **default_options)
+        instance.initialize_code()
+        instance.parameters.time_step = 0.01 | units.yr
+        instance.commit_parameters()
+        instance.particles.add_particles(particles)
+        instance.commit_particles()
+        primary = instance.particles[0]
+        
+        P = 0.1 | units.yr
+        
+        position_at_start = primary.position.x
+        dt = instance.parameters.time_step
+        time = 0 * dt
+        
+        instance.evolve_model(P / 2.0)
+        self.assertAlmostRelativeEqual(0.05 | units.AU, primary.position.x, 3)
+        
+        # instance.evolve_model(P)
+        # self.assertAlmostRelativeEqual(position_at_start, primary.position.x, 3)
+        
+        instance.cleanup_code()
+        instance.stop()
+
+    def test_evolve_twoparticles_a(self):
+        particles = Particles(2)
+        particles.mass = [1.0, 0.001] | units.MSun
+        # instance.parameters.rcut_out_star_star = 10.0 | units.AU
+        # particles.radius = 10.0 | units.AU
+        particles.position = [[0.0, 0.0, 0.0], [10.0, 0.0, 0.0]] | units.AU
+        particles.velocity = [[1.0, 0.0, 0.0], [0.0, 0.0, 0.0]] | units.AU / units.yr
+        
+        converter = nbody_system.nbody_to_si(1.0 | units.MSun, 1.0 | units.AU)
+        instance = Pentacle(converter) #self.new_instance_of_an_optional_code(Pentacle, converter, **default_options)
+        instance.initialize_code()
+        instance.parameters.time_step = 0.01 | units.yr
+        instance.commit_parameters()
+        instance.particles.add_particles(particles)
+        instance.commit_particles()
+        primary = instance.particles[0]
+        
+        P = 0.1 | units.yr
+        
+        position_at_start = primary.position.x
+        dt = instance.parameters.time_step
+        time = 0 * dt
+        
+        instance.evolve_model(P)
+        self.assertAlmostRelativeEqual(primary.position.x, 0.1 | units.AU, 3)
+        
+        # instance.evolve_model(P)
+        # self.assertAlmostRelativeEqual(position_at_start, primary.position.x, 3)
+        
+        instance.cleanup_code()
+        instance.stop()
+
+    def test_evolve_twoparticles(self):
+        # from amuse.community.ph4.interface import ph4
+        # from amuse.community.bhtree.interface import BHTree
+        particles = Particles(2)
+        particles.mass = 1.0 | units.MSun
+        # instance.parameters.rcut_out_star_star = 10.0 | units.AU
+        particles.radius = 0.1 | units.AU
+        particles.position = [[0.0, 0.0, 0.0], [2.0, 0.0, 0.0]] | units.AU
+        particles.velocity = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]] | units.km / units.s
+        particles[1].vy = (constants.G * (2.0 | units.MSun) / (2.0 | units.AU)).sqrt()
+        particles.move_to_center()
+        
+        converter = nbody_system.nbody_to_si(1.0 | units.MSun, 1.0 | units.AU)
+        instance = Pentacle(converter) #self.new_instance_of_an_optional_code(Pentacle, converter, **default_options)
+        instance.initialize_code()
+        instance.parameters.time_step = 0.0125 * math.pi * particles[0].x / particles[0].vy
+        instance.commit_parameters()
+        instance.particles.add_particles(particles)
+        instance.commit_particles()
+        primary = instance.particles[0]
+        
+        P = 2 * math.pi * primary.x / primary.vy
+        
+        position_at_start = primary.position.x
+        dt = instance.parameters.time_step
+        time = 0 * dt
+        instance.evolve_model(P / 4.0)
+        #self.assertAlmostRelativeEqual(position_at_start, primary.position.y, 2)
+        
+        instance.evolve_model(P / 2.0)
+        self.assertAlmostRelativeEqual(position_at_start, -primary.position.x, 2)
+        
+        instance.evolve_model(P)
+        self.assertAlmostRelativeEqual(position_at_start, primary.position.x, 3)
+        
+        instance.cleanup_code()
+        instance.stop()
+
+    def test_earthsun(self):
+        # from amuse.community.ph4.interface import ph4
+        converter = nbody_system.nbody_to_si(1.0 | units.MSun, 1.0 | units.AU)
+        instance = self.new_instance_of_an_optional_code(Pentacle, converter, **default_options)
+        instance.initialize_code()
+        instance.parameters.time_step = 0.005 | units.yr
+        instance.commit_parameters()
+        instance.particles.add_particles(self.new_sun_earth_system())
+        instance.commit_particles()
+        earth = instance.particles[1]
+        
+        position_at_start = earth.position.x
+        instance.evolve_model(0.25 | units.yr)
+        #self.assertAlmostRelativeEqual(position_at_start, earth.position.y, 2)
+        
+        instance.evolve_model(0.5 | units.yr)
+        self.assertAlmostRelativeEqual(position_at_start, -earth.position.x, 2)
+        
+        instance.evolve_model(1.0 | units.yr)
+        self.assertAlmostRelativeEqual(position_at_start, earth.position.x, 2)
+        
+        instance.cleanup_code()
+        instance.stop()
+
