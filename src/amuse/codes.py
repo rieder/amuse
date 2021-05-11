7ceaefd29b2034a52fb3c1ab834b2c61d8b0bc0c
Steven Rieder <steven+github@rieder.nl>
2021-05-11 19:59:57 +0100

    Update codes.py

--- src/amuse/codes.py
+++ src/amuse/codes.py
@@ -1,32 +1,34 @@
-
+"""AMUSE codes
+"""
 import sys
 
 _CODES = [
-'athena', 'capreole', 'cachedse',     'gadget2',      'mesa',         'octgrav',      'twobody',
-'capreole',     'hermite0',     'mocassin',     'phiGRAPE',
-'athena',       'evtwin',       'hop',          'seba',
-'bhtree',       'evtwin2sse',   'interface',    'smallN',
-'bse',          'fi',           'mercury',      'sse',
+    'athena', 'capreole', 'cachedse',     'gadget2',      'mesa',
+    'octgrav',      'twobody', 'capreole',     'hermite0',     'mocassin',
+    'phiGRAPE', 'athena',       'evtwin',       'hop',          'seba',
+    'bhtree',       'evtwin2sse',   'interface',    'smallN', 'bse',
+    'fi',           'mercury',      'sse',
 ]
 
 __all__ = []
 
+
 def _import_modules():
 
-    for x in _CODES:
-        modulename = 'amuse.legacy.' + x + '.interface'
+    for code in _CODES:
+        modulename = 'amuse.legacy.' + code + '.interface'
         try:
             __import__(modulename)
-            globals()[x] = sys.modules[modulename]
-            __all__.append(x)
+            globals()[code] = sys.modules[modulename]
+            __all__.append(code)
         except ImportError as ex:
-            modulename = 'amuse.legacy.' + x + '.' + x
+            modulename = 'amuse.legacy.' + code + '.' + code
             try:
                 __import__(modulename) 
-                globals()[x] = sys.modules[modulename]
-                __all__.append(x)
+                globals()[code] = sys.modules[modulename]
+                __all__.append(code)
             except ImportError as ex:
                 pass
 
+
 _import_modules()
-    
