8d7535841ed85b28323da2e0998c883d56cc1bf4
Steven Rieder <s.rieder@uva.nl>
2025-03-04 14:18:56 +0100

    remove python particleset

--- src/amuse/community/metisse/interface.py
+++ src/amuse/community/metisse/interface.py
@@ -73,10 +73,6 @@ class MetisseInterface(
     def get_main_sequence_lifetime(index_of_the_star="i"):
         returns (main_sequence_lifetime="d" | units.Myr)
 
-    @remote_function(can_handle_array=True)
-    def evolve_for(index_of_the_star="i", time_delta="d" | units.Myr):
-        returns ()
-
     # getters and setters for tracks
     # metallicity_dir (string)
     # metallicity_dir_he (string)
@@ -431,7 +427,7 @@ class Metisse(se.StellarEvolution):
         handler.add_method("particles", "evolve_for")
 
     def evolve_model(self, end_time=None, keep_synchronous=True):
-        print("evolve_model", end_time, keep_synchronous)        
+        print("evolve_model", end_time, keep_synchronous)
         if not keep_synchronous:
             for particle in self.particles:
                 particle.evolve_one_step()
@@ -443,93 +439,7 @@ class Metisse(se.StellarEvolution):
             else 0.99*min(self.particles.time_step)
         )
         print(f"{delta_time=}")
-        for particle in self.particles:
+        for i, particle in enumerate(self.particles):
+            print(f"{i} {particle.age} {particle.mass}")
             particle.evolve_for(particle.age + delta_time)
         self.model_time += delta_time
-
-
-class MetisseParticles(Particles):
-
-    def __init__(self, code_interface, storage=None):
-        Particles.__init__(self, storage=storage)
-        self._private.code_interface = code_interface
-        self.add_calculated_attribute(
-            "temperature",
-            self.calculate_effective_temperature,
-            ["luminosity", "radius"],
-        )
-        self.add_function_attribute(
-            "evolve_one_step", self.particleset_evolve_one_step, self.evolve_one_step
-        )
-        self.add_function_attribute(
-            "evolve_for", self.particleset_evolve_for, self.evolve_for
-        )
-
-    def calculate_effective_temperature(self, luminosity, radius):
-        return (
-            (luminosity / (constants.four_pi_stefan_boltzmann * radius**2)) ** 0.25
-        ).in_(units.K)
-
-    def add_particles_to_store(self, keys, attributes=[], values=[]):
-        if len(keys) == 0:
-            return
-
-        all_attributes = []
-        all_attributes.extend(attributes)
-        all_values = []
-        all_values.extend(values)
-
-        mapping_from_attribute_to_default_value = {
-            "stellar_type": 1 | units.stellar_type,
-            "radius": 0 | units.RSun,
-            "luminosity": 0 | units.LSun,
-            "core_mass": 0 | units.MSun,
-            "CO_core_mass": 0 | units.MSun,
-            "core_radius": 0 | units.RSun,
-            "convective_envelope_mass": 0 | units.MSun,
-            "convective_envelope_radius": 0 | units.RSun,
-            "epoch": 0 | units.Myr,
-            "spin": 0 | units.yr**-1,
-            "main_sequence_lifetime": 0 | units.Myr,
-            "age": 0 | units.Myr,
-        }
-
-        given_attributes = set(attributes)
-
-        if "initial_mass" not in given_attributes:
-            index_of_mass_attibute = attributes.index("mass")
-            all_attributes.append("initial_mass")
-            all_values.append(values[index_of_mass_attibute] * 1.0)
-
-        for attribute, default_value in mapping_from_attribute_to_default_value.items():
-            if attribute not in given_attributes:
-                all_attributes.append(attribute)
-                all_values.append(default_value.as_vector_with_length(len(keys)))
-
-        super().add_particles_to_store(keys, all_attributes, all_values)
-
-        added_particles = ParticlesSubset(self, keys)
-        self._private.code_interface._evolve_particles(added_particles, 0 | units.yr)
-
-    def evolve_one_step(self, particles, subset):
-        self._private.code_interface._evolve_particles(
-            subset.as_set(), subset.age + subset.time_step
-        )
-
-    def particleset_evolve_one_step(self, particles):
-        self._private.code_interface._evolve_particles(
-            particles, particles.age + particles.time_step
-        )
-
-    def evolve_for(self, particles, subset, delta_time):
-        self._private.code_interface._evolve_particles(
-            subset.as_set(), subset.age + delta_time
-        )
-
-    def particleset_evolve_for(self, particles, delta_time):
-        self._private.code_interface._evolve_particles(
-            particles, particles.age + delta_time
-        )
-
-    def get_defined_attribute_names(self):
-        return ["mass", "radius"]
