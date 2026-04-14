e04527c70ba9f04732f3b93b894dda63c1ad0fec
Steven Rieder <s.rieder@uva.nl>
2026-04-14 17:05:21 +0200

    that's all of them, I think

--- support/setup/installing.sh
+++ support/setup/installing.sh
@@ -83,7 +83,7 @@ install_framework() {
             fi
         done
         if [ -n "${to_install}" ] ; then
-            conda install -c conda-forge --override-channels -y ${to_install}
+            conda install -y ${to_install}
         fi
     fi
 
@@ -187,20 +187,16 @@ install_package() {
     check_package "${package}"
 
     if ! is_subset "amuse-framework" "${INSTALLED_PACKAGES}" ; then
-        save_cmd="${cmd}"
         save_package="${package}"
         install_framework
         package="${save_package}"
-        cmd="${save_cmd}"
     fi
 
     if is_subset "${package}" "${NEEDS_SAPPORO_LIGHT}" ; then
         if ! is_subset "sapporo-light" "${INSTALLED_PACKAGES}" ; then
-            save_cmd="${cmd}"
             save_package="${package}"
             install_sapporo_light
             package="${save_package}"
-            cmd="${save_cmd}"
         fi
     fi
 
@@ -211,11 +207,9 @@ install_package() {
         # If the code is e.g. CUDA-only, then there may not be a base package.
         if is_subset "${base_package}" "${EXTANT_PACKAGES}" ; then
             if ! is_subset "${base_package}" "${INSTALLED_PACKAGES}" ; then
-                save_cmd="${cmd}"
                 save_package="${package}"
                 install_package "${cmd}" "${base_package}" "${brief}"
                 package="${save_package}"
-                cmd="${save_cmd}"
             fi
         fi
     fi
--- support/setup/testing.sh
+++ support/setup/testing.sh
@@ -7,7 +7,7 @@ ensure_pytest() {
             printf '%s\n\n' "Please activate a conda environment or virtual environment first."
         elif [ "a${ENV_TYPE}" = "aconda" ] ; then
             printf '\n%s\n' "Please use"
-            printf '\n    %b\n' "conda install -c conda-forge --override-channels pytest"
+            printf '\n    %b\n' "conda install pytest"
             printf '\n%s\n\n' "to install pytest, then try again."
         else
             printf '\n%s\n' "Please use"
@@ -50,6 +50,7 @@ test_framework() {
             cd src/tests && pytest --import-mode=append core_tests compile_tests --ignore compile_tests/java_implementation -k 'not TestCDistributedImplementationInterface and not TestAsyncDistributed' ${PYTEST_OPTS}
         else
             cd src/tests && pytest --import-mode=append core_tests compile_tests --ignore compile_tests/java_implementation -k 'not TestCDistributedImplementationInterface and not TestAsyncDistributed and not noci' ${PYTEST_OPTS}
+
         fi
 
         echo $? >"../../${ec_file}"
@@ -80,7 +81,7 @@ test_amuse_ext() {
     log_file="$(log_file test amuse-ext)"
 
     (
-        cd src/tests && pytest ext_tests ticket_tests --import-mode=append ${PYTEST_OPTS} -s -v -x -k "not noci and ${bad_ext_tests}"
+        cd src/tests && pytest ext_tests --import-mode=append ticket_tests ${PYTEST_OPTS}  -k "${bad_ext_tests}"
 
         echo $? >"../../${ec_file}"
     ) 2>&1 | tee "${log_file}"
