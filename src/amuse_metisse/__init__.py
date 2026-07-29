47d37589436a3a9d657d78b785e8effee1837ed7
Steven Rieder <s.rieder@uva.nl>
2026-07-29 17:05:26 +0200

    Download data files on first run

--- src/amuse_metisse/__init__.py
+++ src/amuse_metisse/__init__.py
@@ -1,5 +1,13 @@
 """
 Interface for METISSE
 """
+import os
 from .interface import MetisseInterface
 from .interface import Metisse
+from .download import download_sample_metisse_tracks
+
+if not os.path.exists(os.path.join(os.path.dirname(__file__), "data")):
+    print("Downloading sample MESA tracks for METISSE")
+    download_sample_metisse_tracks()
+
+__all__ = ["MetisseInterface", "Metisse"]
